1fdd4834f50e7176d1c770a40085578610478523
DepresivoSG <81449482+Juan-Alayon@users.noreply.github.com>
2022-05-13 23:59:14 -0500

    Update DML_lol.sql

--- Entrega2/DML_lol.sql
+++ Entrega2/DML_lol.sql
@@ -1,21 +1,19 @@
 COPY Worlds (year,role)
-	FROM 'Worlds.csv' 
+	FROM 'E:\Descargas\Datos\proyecyo_cool\worlds_cosas.csv' 
 		WITH DELIMITER ';' 
 		CSV HEADER;
 
-select * from worlds;
-
-COPY Champion(name,class,play_style,title)
-	FROM 'Champion.csv' 
+COPY champion(name,class,play_style,title)
+	FROM 'E:\Descargas\Datos\proyecyo_cool\Champion.csv' 
 		WITH DELIMITER ';' 
 		CSV HEADER;
 
 COPY Ability(name_champion,name,key,base_range,base_cooldown)
-	FROM 'Ability.csv' 
+	FROM 'E:\Descargas\Datos\proyecyo_cool\Ability.csv' 
 		WITH DELIMITER ';' 
 		CSV HEADER;
 
 COPY Play(year_Worlds,name_Champion, role_Worlds, pick_rate, ban_rate, win_rate, kills, deaths)
-	FROM 'Play.csv' 
+	FROM 'E:\Descargas\Datos\proyecyo_cool\play.csv' 
 		WITH DELIMITER ';' 
 		CSV HEADER;
